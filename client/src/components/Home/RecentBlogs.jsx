685bb58356fbe38e2f3025f4965df0e8fd3b4f20
diwakaraman <2023aspire53@gmail.com>
2025-04-14 12:22:03 +0530

    complete home page UI

--- client/src/components/Home/RecentBlogs.jsx
+++ client/src/components/Home/RecentBlogs.jsx
@@ -1,29 +1,33 @@
 import React from 'react'
+import BlogCard from '../Blog Card/BlogCard';
 
 const RecentBlogs = () => {
   const data = [
     {img: "./temp.jpg",
     title: "Lorem ipsum ",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus."},
+    description: "lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus. lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus."},
 
     {img: "./temp.jpg",
       title: "Lorem ipsum .",
-      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus."},
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus. lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus. lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus."},
 
       {img: "./temp.jpg",
         title: "Lorem ipsum .",
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus."},
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus. lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus. lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus."},
   ];
 
   return (
     <div className='mb-4 py-4'>
        <h1 className='text-xl font-semibold mb-4'>RecentBlogs</h1>
-       <div className='flex flex-col gap-4'>
+       <div className='flex flex-col  gap-8 lg:gap-4'>
 {data &&
 data.map((items,i) => (
-  <div key={i}>
-    <h3>{items.title}</h3>
-    <p>{items.description}</p>
+  <div key={i} className='flex flex-col lg:flex-row gap-2 lg:gap-4'>
+   
+    {/* <h3>{items.title}</h3>
+    <p>{items.description}</p> */}
+   
+    <BlogCard items={items}/>
   </div>
 ))}
         
